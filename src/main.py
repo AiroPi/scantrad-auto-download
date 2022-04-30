9e122c71aab16c98be642813af2b046093552344
airo.pi_ <47398145+AiroPi@users.noreply.github.com>
2022-04-30 23:36:02 +0200

    Change rss in favor of unofficial API, and setup download

--- src/main.py
+++ src/main.py
@@ -1,51 +1,61 @@
-# https://sf.ldgr.fr/api/
-# https://sf.ldgr.fr/api/chapters/one-piece
-
 import os
-from typing import TYPE_CHECKING, Generator, Any
+import time
 import re
+from glob import glob
 from functools import partial
 
 from mloader import loader
 from mloader.exporter import RawExporter
 import requests
-import feedparser
 
 INTERVAL = 60
 
-if TYPE_CHECKING:
-    from feedparser import FeedParserDict
+mangas = ['one-piece']
 
 
-def get_chapters(parsed_feed: "FeedParserDict") -> Generator[tuple[int, Any], None, None]:
-    for entry in parsed_feed.entries:
-        if result := re.search(r'\d+$', entry.link):
-            chapter_number = int(result.group(0))
-        else:
+def update_mangas():
+    for manga in mangas:
+        try:
+            chapters = requests.get(f"https://sf.ldgr.fr/api/chapters/{manga}").json()
+        except Exception:
+            print(f"Error: {manga}")
             continue
 
-        yield chapter_number, entry
+        for chapter in chapters:
+            destination = f"./output/{manga}/{chapter['number']}"
+
+            if os.path.isdir(destination):
+                continue
+            os.makedirs(destination)
+
+            if not (result := re.search(r"\d+$", chapter['source'])):
+                continue
+            mangaplus_id = int(result.group(0))
+
+            exporter = partial(RawExporter, destination="./tmp", title="", add_chapter_title=False)
+            manga_loader = loader.MangaLoader(exporter)
+
+            manga_loader.download(
+                title_ids=None,
+                chapter_ids={mangaplus_id},
+                min_chapter=int(chapter['number']),
+                max_chapter=int(chapter['number']),
+                last_chapter=False
+            )
+
+            for i, file in enumerate(sorted(glob("./tmp/*/*.jpg"))):
+                os.rename(file, f"{destination}/{i}.jpg")
+
+            for folder in glob("./tmp/*"):
+                os.rmdir(folder)
+
+            print(f"New chapiter downloaded for {manga} : {chapter['number']}")
 
 
 def main():
-    parsed_feed: "FeedParserDict" = feedparser.parse('https://scantrad.net/rss/one-piece')
-
-    print(f"Parsing {parsed_feed.feed.title} at {parsed_feed.feed.link}")
-
-    print(parsed_feed.entries[0].keys())
-    for chapter_number, entry in get_chapters(parsed_feed):
-        destination = f"./one-piece/{chapter_number}"
-        if os.path.isdir(destination): continue
-
-        os.makedirs(destination)
-
-        exporter = partial(RawExporter, destination=destination, add_chapter_title=False)
-        manga_loader = loader.MangaLoader(exporter)
-
-        print(f"{chapter_number} - {entry.title}")
-    # Récupération du dernier feed
-    # if len(news_feed.entries):
-    #     print("Alert: {} \nLink: {}".format(news_feed.entries[0].title, news_feed.entries[0].link))
+    while True:
+        update_mangas()
+        time.sleep(INTERVAL)
 
 
 if __name__ == "__main__":
