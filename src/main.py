e0904ab3adac8c427726afb6e95fdb583a1753e4
airo.pi_ <47398145+AiroPi@users.noreply.github.com>
2022-05-01 12:58:40 +0200

    Send webhook message on new chapter, and download to mega.nz

--- src/main.py
+++ src/main.py
@@ -8,6 +8,22 @@ from mloader import loader
 from mloader.exporter import RawExporter
 import requests
 
+try:
+    from mega import Mega
+    mega = Mega()
+    m = mega.login(os.environ['EMAIL'], os.environ['PASSWORD'])
+    root = m.find('scans/', exclude_deleted=True)
+    if not root:
+        root = m.create_folder('scans')['scans']
+    else:
+        root = root[0]
+except Exception:
+    MEGA_UPLOAD = False
+    print('bug')
+else:
+    MEGA_UPLOAD = True
+
+
 INTERVAL = 60
 
 mangas = ['one-piece']
@@ -20,6 +36,13 @@ def update_mangas():
         except Exception:
             print(f"Error: {manga}")
             continue
+
+        if MEGA_UPLOAD:
+            parent = m.find(f"scans/{manga}/", exclude_deleted=True)
+            if not parent:
+                parent = m.create_folder(f"{manga}", root)[manga]
+            else:
+                parent = parent[0]
 
         for chapter in chapters:
             destination = f"./output/{manga}/{chapter['number']}"
@@ -43,11 +66,19 @@ def update_mangas():
                 last_chapter=False
             )
 
+            if MEGA_UPLOAD:
+                folder = m.create_folder(f"{chapter['number']}", parent)[str(chapter['number'])]
+
             for i, file in enumerate(sorted(glob("./tmp/*/*.jpg"))):
                 os.rename(file, f"{destination}/{i}.jpg")
+                if MEGA_UPLOAD:
+                    mega.upload(f"{destination}/{i}.jpg", folder)
 
             for folder in glob("./tmp/*"):
                 os.rmdir(folder)
+
+            if os.environ["DISCORD_WEBHOOK"]:
+                requests.post(os.environ["DISCORD_WEBHOOK"], json={"content": f"Nouveau chapitre {manga} ! {chapter['number']}\nGo le lire ici : {chapter['source']}"})
 
             print(f"New chapiter downloaded for {manga} : {chapter['number']}")
 
