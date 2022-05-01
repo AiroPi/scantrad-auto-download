a234ecd1346795ac999673e10526c9f7e8597de7
airo.pi_ <47398145+AiroPi@users.noreply.github.com>
2022-05-01 15:49:02 +0200

    Dockerize

---
+++ start.sh
@@ -0,0 +1,5 @@
+#!/bin/bash
+git pull origin master
+docker build -t scantrad-auto-download .
+docker rm -f scantrad-auto-download || true
+docker run --name scantrad-auto-download -d --restart="always" --env-file=.env .
